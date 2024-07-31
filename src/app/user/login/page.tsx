4eaf98a8f199ee16882fdfa2ca64197bc0981f8e
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-07-31 22:34:23 +0530

    page

---
+++ src/app/user/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Login from "@/app/(components)/Login";
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/user/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import SignUp from "@/app/(components)/SignUp";
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+
+export default Page;
