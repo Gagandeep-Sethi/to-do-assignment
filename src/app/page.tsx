d6f16508684d4066e8a431e2e74c8b8b49d54eb3
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-07-31 22:31:33 +0530

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import CreateTask from "./(components)/CreateTask";
+import Login from "./(components)/Login";
+import SignUp from "./(components)/SignUp";
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <CreateTask />
+    </div>
+  );
 }
