220677f74fb521511db37215e09f579793b9e004
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-08-02 17:34:55 +0530

    edit task route

---
+++ src/app/api/task/editTask/[id]/route.ts
@@ -0,0 +1,56 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextResponse, NextRequest } from "next/server";
+import { getTokenData } from "@/helpers/getTokenData";
+import Task from "@/models/taskModel";
+
+connect();
+
+export async function POST(req: NextRequest, { params }: any) {
+  try {
+    console.log(params, "params");
+    const body = await req.json();
+    const { title, description, status, priority, deadline } = body;
+
+    const userId = await getTokenData(req);
+
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error(`user not found`);
+    }
+    const task = await Task.findById(params.id);
+    if (!task) {
+      throw new Error(`task you are trying to edit no longer exist`);
+    }
+    if (task.userId.toString() !== userId.toString()) {
+      throw new Error(
+        `user trying to edit the task is not creater of the task`
+      );
+    }
+
+    const updatedProduct = await Task.findByIdAndUpdate(
+      params.id,
+      {
+        title,
+        description,
+        status,
+        priority,
+        deadline,
+        userId: user._id,
+      },
+      { new: true }
+    );
+    const response = NextResponse.json(
+      { message: "Task updated", updatedProduct },
+      { status: 200 }
+    );
+
+    return response;
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    } else {
+      return NextResponse.json({ message: "server error" }, { status: 500 });
+    }
+  }
+}
